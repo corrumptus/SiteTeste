45f2a563c79eeac3ace241de3424406a011e73d4
corrumptus <lazarinilucas42@gmail.com>
2024-05-22 20:43:22 -0300

    moving server.js script to the src/index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,21 @@
 import express from 'express';
+import cors from 'cors';
+import sorvete from './Controllers/Sorveteria.js';
+
 const app = express();
 
-export default app;
+const port = 3000;
+
+app.use(cors());
+
+app.listen(port,()=>
+{
+    console.log(`Servidor rodando no endereço http://localhost:${port}`);
+})
+
+app.use(sorvete);
+
+app.get('/',(req,res)=>
+{
+    res.status(200).send("Sistema de sorveteria em node js");
+});
